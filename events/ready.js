ed6de9150a8cfc1a6450cffc64103fe436b99072
JLM0ha <103145585+JLM0ha@users.noreply.github.com>
2022-04-19 22:28:44 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 const client = require("../index.js")
 client.on("ready", () => {
-  console.log("Ticket Bot is Ready Th | N A S S E R ♕︎©")
+  console.log("Ticket Bot By Eternity RP")
   client.user.setPresence({
     status: 'idle',
     activities: [{ 
